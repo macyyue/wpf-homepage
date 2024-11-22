0aac89d6123fb5e7add9557943cda591a367d5df
macyyue <144971569+macyyue@users.noreply.github.com>
2024-11-22 16:37:31 +0900

    main Update

--- wp-content/themes/usfactoryOriginal/index.php
+++ wp-content/themes/usfactoryOriginal/index.php
@@ -25,93 +25,162 @@ get_header(); ?>
   <div><img src="/common/img/homepage/top-page_04.png" alt="" /></div>
 </div>
 <!-- 追加 -->
-<article id="mainWrap">
-  <article id="contentsWrap" class="topWrap">
-    <article id="contents">
-      <div class="releaseWrap">
-        <div class="sectionHeader">
-          <span>リリース</span>
+ <div id="container">
+ <main>
+    <!-- service -->
+    <section>
+      <h2>Service<span>サービス</span></h2>
+      <div class="list-container">
+        <div class="list blur">
+          <figure>
+            <img
+              loading="lazy"
+              decoding="async"
+              src="/common/img/homepage/top-page_BIforAC_01.png" alt="">
+          </figure>
+          <div class="text">
+            <h4>BI for AC<span class="trademark">®</span></h4>
+            <p>BIM積算の決定版!建材の自動発生~集計に加え、施工検討や工程シミュレーションが出来るArchicad<span class="trademark">®</span>プラグインソフトです。</p>
+          </div>
+          <p class="btn"><a href="/biforac/">詳細はコチラ</a></p>
+          <!-- <span class="icon">NEW</span> -->
         </div>
-        <?php $args = array(
-          'post_type' => 'release',
-          'posts_per_page' => 10,
-        ); ?>
-        <?php $loop = new WP_Query($args); ?>
-        <?php if ($loop->have_posts()) : ?>
-          <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-            <dl>
-              <dt><?php the_date("Y年n月j日"); ?></dt>
-              <dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
-            </dl>
-          <?php endwhile; ?>
-        <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
+        <div class="list blur">
+          <figure>
+            <img src="/common/img/homepage/top-page_BIforAC_mokuzo_01.png"
+              alt=""
+              loading="lazy"
+              decoding="async">
+          </figure>
+          <div class="text">
+            <h4>Bl for AC<span class="trademark">®</span> x木造住宅</h4>
+            <p>CEDXMデータを読込んで建物モデルを自動作成する機能や、構造金物の自動発生など、木造住宅に特化した機能を集約しました。</p>
+          </div>
+          <p class="btn"><a href="/mokuzou/">詳細はコチラ</a></p>
+        </div>
+        <div class="list blur">
+          <figure><img src="/common/img/homepage/top-page_Info360_01.png"
+              alt=""
+              loading="lazy"
+              decoding="async">
+          </figure>
+          <div class="text">
+            <h4>Info360<span class="trademark">®</span></h4>
+            <p>点群データやIFCモデル等の3Dや、図面等の資料をWEBで表示共有が出来るデジタルツインです。BCFも兼ね備え、現場管理に活用出来ます。</p>
+          </div>
+          <p class="btn"><a href="/info360/">詳細はコチラ</a></p>
+        </div>
+        <div class="list blur">
+          <figure><img src="/common/img/homepage/top-page_tengun_01.png"
+              alt=""
+              loading="lazy"
+              decoding="async">
+          </figure>
+          <div class="text">
+            <h4>点群計測サービス</h4>
+            <p>高精度3Dレーザー測量機による建築物等の計測～点群合成～BIM作成まで担うー貫サポートです。測量機のレンタルサービスも行います。</p>
+          </div>
+          <p class="btn"><a href="event.html">詳細はコチラ</a></p>
+        </div>
       </div>
-      <div class="newsWrap">
-        <div class="sectionHeader">
-          <span>イベント</span>
-        </div>
-        <?php $args = array(
-          'post_type' => 'news',
-          'posts_per_page' => 5,
-        ); ?>
-        <?php $loop = new WP_Query($args); ?>
-        <?php if ($loop->have_posts()) : ?>
-          <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-            <dl>
-              <dt><?php the_date("Y年n月j日"); ?></dt>
-              <dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
-            </dl>
-          <?php endwhile; ?>
-        <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
-        <div class="viewMore">
-          <a href="<?php echo esc_url(home_url('news')); ?>">過去のイベントを見る</a>
-        </div>
-      </div>
+    </section>
+    <section>
+      <article id="mainWrap">
+        <article id="contents">
+          <div class="releaseWrap">
+            <h2>Release<span>リリース情報</span></h2>
+            <?php $args = array(
+              'post_type' => 'release',
+              'posts_per_page' => 3,
+            ); ?>
+            <?php $loop = new WP_Query($args); ?>
+            <?php if ($loop->have_posts()) : ?>
+              <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                <dl>
+                  <dt><?php the_date("Y年n月j日"); ?></dt>
+                  <dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+                </dl>
+              <?php endwhile; ?>
+            <?php endif; ?>
+            <?php wp_reset_postdata(); ?>
+            <div class="viewMore">
+              <a href="<?php echo esc_url(home_url('release')); ?>">過去のリリースを見る</a>
+            </div>
+          </div>
+          <div class="newsWrap">
+            <h2>Event<span>イベント情報</span></h2>
+            <?php $args = array(
+              'post_type' => 'news',
+              'posts_per_page' => 3,
+            ); ?>
+            <?php $loop = new WP_Query($args); ?>
+            <?php if ($loop->have_posts()) : ?>
+              <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                <dl>
+                  <dt><?php the_date("Y年n月j日"); ?></dt>
+                  <dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+                </dl>
+              <?php endwhile; ?>
+            <?php endif; ?>
+            <?php wp_reset_postdata(); ?>
+            <div class="viewMore">
+              <a href="<?php echo esc_url(home_url('news')); ?>">過去のイベントを見る</a>
+            </div>
+          </div>
+          <div class="medialistWrap">
+            <h2>Media<span>メディア情報</span></h2>
+            <?php $args = array(
+              'post_type' => 'media',
+              'posts_per_page' => 3,
+            ); ?>
+            <?php $loop = new WP_Query($args); ?>
+            <?php if ($loop->have_posts()) : ?>
+              <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                <dl>
+                  <dt><?php the_date("Y年n月j日"); ?></dt>
+                  <dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+                </dl>
+              <?php endwhile; ?>
+            <?php endif; ?>
+            <?php wp_reset_postdata(); ?>
+            <div class="viewMore">
+              <a href="<?php echo esc_url(home_url('media')); ?>">過去のメディアを見る</a>
+            </div>
+          </div>
+        </article>
+        <!-- sidebar -->
+        <aside id="sidebar">
+        <h2>Information<span>会社情報</span></h2>
+          <div id="sub">
+          <address class="box">
+              <!-- <h2>BIM建設講座</h2> -->
+              <a href="https://events.teams.microsoft.com/event/d2f041f0-2124-4e4d-ad6f-16f1ae81e6f8@9143b3f8-6101-47ea-bf82-d9e923b6a88e" target="_blank">
+                <img src="common/img/20241128_05_webinar.png" />
+              </a>
+              <a href="/inquiry/" target="_blank">
+                <img src="common/img/banner_campaign_AI-Structure_20241010.png" />
+              </a>
+              <!-- <p class="check">AIで建物モデルを自動作成する業務革命。
+                <br>AI-Structureでつくる超簡単な3Dモデル作成方法とは?
+              </p>
+              <p>
+                講師：上嶋　泰史
+                <br>※参加無料
+              </p> -->
+            </address>
+            <ul class="submenu">
+              <li><a href="/interview01/">トップメッセージ<span>Top message</span></a></li>
+              <li><a href="/company/">会社概要<span>Information</span></a></li>
+              <li><a href="/recruit/">採用情報<span>Recruit</span></a></li>
+              <li><a href="/inquiry/">お問い合わせ<span>Contact</span></a></li>
+            </ul>
+          </div>
+        </aside>
+      </article>
+    </section>
+  </main>
+ </div>
 
-      <div class="mediaWrap">
-        <div class="sectionHeader">
-          <span>メディア</span>
-        </div>
-        <?php $args = array(
-          'post_type' => 'media',
-          'posts_per_page' => 4,
-        ); ?>
-        <?php $loop = new WP_Query($args); ?>
-        <?php if ($loop->have_posts()) : ?>
-          <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-            <dl>
-              <dt><?php the_date("Y年n月j日"); ?></dt>
-              <dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
-            </dl>
-          <?php endwhile; ?>
-        <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
-        <div class="viewMore">
-          <a href="<?php echo esc_url(home_url('media')); ?>">過去のメディアを見る</a>
-        </div>
-      </div>
-    </article>
-    <aside id="sidebar">
-      <div class="topLinkBox">
-        <ul>
-        <li><a href="https://events.teams.microsoft.com/event/bf416dd7-e311-4fbc-88b6-877b1d3c228e@9143b3f8-6101-47ea-bf82-d9e923b6a88e" target="_blank">
-          <img src="common/img/20240731_01_webinar.png" width="320" height="160" />
-        </a>
-      </li>
-          <li><a href="/company/"><img src="common/img/ftNav01.jpg" width="300" height="120" alt=""></a></li>
-          <li><a href="/inquiry/"><img src="common/img/ftNav03.jpg" width="320" height="120" alt=""></a></li>
-          <li><a href="/recruit/"><img src="common/img/ftNav04.jpg" width="320" height="120" alt=""></a></li>
-          <li><a href="https://us-factory.jp/media/companytank202209/"><img src="common/img/ct-banner.jpg" alt="" width="320" height="190" /></a></li>
-          <li><a href="/recruit/interview01/"><img src="/common/img/int/interview01.png" /></a></li>
-          <li><a href="/recruit/interview02/"><img src="/common/img/int/interview02.png" /></a></li>
-          <!--<li><a href="http://superceo.jp/book/company/i14012/#!3" target="_blank"><img src="http://superceo.jp/book/company/i14012/bnr/i14012.jpg" alt="次世代経営者のCEOスタイルマガジンSUPERCEOで当社特集掲載中" width="320" height="160" /></a></li>-->
-        </ul>
-      </div>
-    </aside>
-  </article>
-</article>
 <?php get_footer(); ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
 <script>
