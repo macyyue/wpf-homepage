e895e7492b902f15cf2ba367ce9798f69493b095
macyyue <144971569+macyyue@users.noreply.github.com>
2024-11-22 17:08:43 +0900

    Footer Update

--- wp-content/themes/usfactoryOriginal/footer.php
+++ wp-content/themes/usfactoryOriginal/footer.php
@@ -9,39 +9,38 @@
 ?>
 
 <footer>
-<h5><a href="/"><img src="/common/img/ftLogo.png" width="200" height="40" alt=""></a></h5>
-<p class="credit">Copyright©2013-2024　U’sFactory All Rights Reserved.</p>
-
-<div class="ftBox">
-<div class="ftBoxL">
-<h6>株式会社U'sFactory　（ユーズファクトリ）</h6>
-<p class="ftTel">045-348-1560</p>
-<p class="time">受付時間 9：30～17：30（土・日・祝日除く）</p>
-</div>
-<div class="ftBoxR">
-<dl class="ftLink01">
-<dt><a href="/">HOME</a></dt>
-</dl>
-<dl class="ftLink02">
-<dt>INFO360</dt>
-<dd>∟<a href="/robot/">商品説明</a></dd>
-<dd>∟<a href="/inquiry/">お問い合わせ</a></dd>
-</dl>
-<dl class="ftLink03">
-<dt>BI for ArchiCAD</dt>
-<dd>∟<a href="/bi/">商品説明</a></dd>
-<dd>∟<a href="/inquiry/">お問い合わせ</a></dd>
-<dd>∟<a href="https://lms.us-factory.jp/mypage/login">ログイン</a></dd>
-</dl>
-<dl class="ftLink04">
-<dt><a href="/policy/">サイトポリシー</a></dt>
-<dt><a href="/news/">NEWS</a></dt>
-<dt><a href="/media/">実績・メディア掲載</a></dt>
-<dt><a href="/company/">会社概要</a></dt>
-<dt><a href="/access/">アクセス</a></dt>
-</dl>
-</div>
-</div>
+	<div class="footerItem">
+		<div class="ftBoxL">
+		<h5><a href="/"><img src="/common/img/homepage/us-logo.svg" width="200" height="40" alt=""></a></h5>
+				<h6>株式会社U'sFactory　（ユーズファクトリ）</h6>
+				<p class="ftTel">045-348-1560</p>
+				<p class="time">受付時間 9：30～17：30（土・日・祝日除く）</p>
+			</div>
+			<div class="ftBoxR">
+				<dl class="ftLink01">
+					<dt><a href="/">HOME</a></dt>
+				</dl>
+				<dl class="ftLink02">
+					<dt>INFO360</dt>
+					<dd>∟<a href="/robot/">商品説明</a></dd>
+					<dd>∟<a href="/inquiry/">お問い合わせ</a></dd>
+				</dl>
+				<dl class="ftLink03">
+					<dt>BI for ArchiCAD</dt>
+					<dd>∟<a href="/bi/">商品説明</a></dd>
+					<dd>∟<a href="/inquiry/">お問い合わせ</a></dd>
+					<dd>∟<a href="https://lms.us-factory.jp/mypage/login">ログイン</a></dd>
+				</dl>
+				<dl class="ftLink04">
+					<dt><a href="/policy/">サイトポリシー</a></dt>
+					<dt><a href="/news/">NEWS</a></dt>
+					<dt><a href="/media/">実績・メディア掲載</a></dt>
+					<dt><a href="/company/">会社概要</a></dt>
+					<dt><a href="/access/">アクセス</a></dt>
+				</dl>
+			</div>
+		<p class="credit">Copyright©2013-2024　U’sFactory All Rights Reserved.</p>
+	</div>
 </footer>
 
 <!-- /#all_container --></article>
