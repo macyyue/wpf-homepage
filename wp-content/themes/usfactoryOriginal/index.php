6adb7e369d815e29b61438e7d10afebf950569aa
macyyue <144971569+macyyue@users.noreply.github.com>
2024-11-26 15:03:39 +0900

    inview Update

--- wp-content/themes/usfactoryOriginal/index.php
+++ wp-content/themes/usfactoryOriginal/index.php
@@ -193,3 +193,6 @@ get_header(); ?>
 <!--スライドショー（slick）-->
 <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="/common/js/slick.js"></script>
+<!--パララックス（inview）-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/protonet-jquery.inview/1.1.2/jquery.inview.min.js"></script>
+<script src="/common/js/jquery.inview_set.js"></script>
