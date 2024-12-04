75f3ff7e2ed9006d23a3b82602f4994f4597f5ca
macyyue <144971569+macyyue@users.noreply.github.com>
2024-12-04 18:44:34 +0900

    Recruit Font Update

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'homepage' );
+define( 'DB_NAME', 'wpf-homepage' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'wp_root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '123456' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
